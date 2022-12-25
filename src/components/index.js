bb361057b0dcf7505ce5c6150791b2383a06796f
VictorCrisostomo <victorcrisostomoliveira@gmail.com>
2022-12-25 18:24:16 -0300

    create pages & components

---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import "./styles.css";
+
+const index = () => {
+  return (
+    <header>
+        <nav>
+            <ul>
+                <li>Filmes</li>
+                <li>Séries</li>
+                <li>Lançamentos</li>
+                <li>Coleções</li>
+                <li>Pedidos</li>
+                <li>Torrents</li>
+                <li>Cursos</li>
+            </ul>
+        </nav>
+    </header>
+  )
+}
+
+export default index
---
+++ src/pages/Pedidos/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const index = () => {
+  return (
+    <div>
+        <h1>Pedidos</h1>
+    </div>
+  )
+}
