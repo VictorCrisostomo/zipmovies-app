0e7d1f65fd00e0a96e6b92ce4ff7adb8e02353be
VictorCrisostomo <victorcrisostomoliveira@gmail.com>
2023-03-14 22:07:39 -0300

    banners of colections

--- src/components/CardColections.js
+++ src/components/CardColections.js
@@ -1,8 +1,35 @@
 import React from 'react'
 import { Link } from "react-router-dom"
 
+import hp from "../images/HP.jpg"
+import jv from "../images/jogos-vorazes.jpg"
+import jw from "../images/jonh-wick.jpg"
+import jp from "../images/jurassic-park.jpg"
+import msi from "../images/msi.jpg"
+import rb from "../images/rocky-balboa.jpg"
+import sa from "../images/senhor-dos-aneis.jpg"
+import sw from "../images/star-wars.jpg"
+import tm from "../images/transformers.jpg"
+import ucm from "../images/ucm.jpg"
+import vf from "../images/velozes-furiosos.jpg"
+
 
 const CardColections = () => {
+
+    const images = [
+        hp,
+        jv,
+        jw,
+        jp,
+        msi,
+        rb,
+        sa,
+        sw,
+        tm,
+        ucm,
+        vf
+    ]
+
   return (
     <section className='carousel-container'>
             <div className="carousel-header">
@@ -14,7 +41,9 @@ const CardColections = () => {
             <Link to="/colecoes/ucm">
                 <article className='card'>
                     <div className="film-colection">
-                        <h3>UCM</h3>
+
+                        <img key={images[9]} src={images[9]} alt="" />
+
                         {/* {movieList.map((movie, index) =>(
                             <img key={index} src={"https://zip-images.vercel.app"+movie.items.filmes[0].images.poster} alt="" />
                         ))} */}
@@ -25,7 +54,9 @@ const CardColections = () => {
             <Link to="/colecoes/harry-potter">
                 <article className='card'>
                     <div className="film-colection">
-                    <h3>Harry Potter</h3>
+
+                    <img key={images[0]} src={images[0]} alt="" />
+
                         {/* {movieList.map((movie, index) =>(
                             <img key={index} src={"https://zip-images.vercel.app"+movie.items.filmes[2].images.poster} alt="" />
                         ))} */}
@@ -36,7 +67,9 @@ const CardColections = () => {
             <Link to="/colecoes/o-senhor-dos-aneis">
                 <article className='card'>
                     <div className="film-colection">
-                    <h3>O Senhor dos Anéis</h3>
+
+                    <img key={images[6]} src={images[6]} alt="" />
+
                         {/* {movieList.map((movie, index) =>(
                             <img key={index} src={"https://zip-images.vercel.app"+movie.items.filmes[3].images.poster} alt="" />
                         ))} */}
@@ -47,7 +80,9 @@ const CardColections = () => {
             <Link to="/colecoes/jurassic-park">
                 <article className='card'>
                     <div className="film-colection">
-                    <h3>Jurassic Park</h3>
+
+                    <img key={images[3]} src={images[3]} alt="" />
+
                         {/* {movieList.map((movie, index) =>(
                             <img key={index} src={"https://zip-images.vercel.app"+movie.items.filmes[4].images.poster} alt="" />
                         ))} */}
@@ -58,7 +93,9 @@ const CardColections = () => {
             <Link to="/colecoes/jogos-vorazes">
                 <article className='card'>
                     <div className="film-colection">
-                    <h3>Jogos Vorazes</h3>
+
+                    <img key={images[1]} src={images[1]} alt="" />
+
                         {/* {movieList.map((movie, index) =>(
                             <img key={index} src={"https://zip-images.vercel.app"+movie.items.filmes[5].images.poster} alt="" />
                         ))} */}
@@ -69,7 +106,9 @@ const CardColections = () => {
             <Link to="/colecoes/velozes-e-furiosos">
                 <article className='card'>
                     <div className="film-colection">
-                    <h3>Velozes e Furiosos</h3>
+
+                    <img key={images[10]} src={images[10]} alt="" />
+
                         {/* {movieList.map((movie, index) =>(
                             <img key={index} src={"https://zip-images.vercel.app"+movie.items.filmes[6].images.poster} alt="" />
                         ))} */}
@@ -80,7 +119,9 @@ const CardColections = () => {
             <Link to="/colecoes/transformers">
                 <article className='card'>
                     <div className="film-colection">
-                        <h3>Transformers</h3>
+
+                        <img key={images[8]} src={images[8]} alt="" />
+
                         {/* {movieList.map((movie, index) =>(
                             <img key={index} src={"https://zip-images.vercel.app"+movie.items.filmes[7].images.poster} alt="" />
                         ))} */}
@@ -91,7 +132,9 @@ const CardColections = () => {
             <Link to="/colecoes/jonh-wick">
                 <article className='card'>
                     <div className="film-colection">
-                    <h3>Jonh Wick</h3>
+
+                    <img key={images[2]} src={images[2]} alt="" />
+
                         {/* {movieList.map((movie, index) =>(
                             <img key={index} src={"https://zip-images.vercel.app"+movie.items.filmes[8].images.poster} alt="" />
                         ))} */}
@@ -102,7 +145,9 @@ const CardColections = () => {
             <Link to="/colecoes/star-wars">
                 <article className='card'>
                     <div className="film-colection">
-                        <h3>Star Wars</h3>
+
+                        <img key={images[7]} src={images[7]} alt="" />
+
                         {/* {movieList.map((movie, index) =>(
                             <img key={index} src={"https://zip-images.vercel.app"+movie.items.filmes[9].images.poster} alt="" />
                         ))} */}
@@ -113,7 +158,8 @@ const CardColections = () => {
             <Link to="/colecoes/missao-impossivel">
                 <article className='card'>
                     <div className="film-colection">
-                    <h3>Missão Impossivel</h3>
+
+                    <img key={images[4]} src={images[4]} alt="" />
                         {/* {movieList.map((movie, index) =>(
                             <img key={index} src={"https://zip-images.vercel.app"+movie.items.filmes[10].images.poster} alt="" />
                         ))} */}
@@ -124,7 +170,9 @@ const CardColections = () => {
             <Link to="/colecoes/rocky-balboa">
                 <article className='card'>
                     <div className="film-colection">
-                    <h3>Rocky Balboa</h3>
+
+                    <img key={images[5]} src={images[5]} alt="" />
+                    
                         {/* {movieList.map((movie, index) =>(
                             <img key={index} src={"https://zip-images.vercel.app"+movie.items.filmes[10].images.poster} alt="" />
                         ))} */}
